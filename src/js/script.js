0c95b9295b39192d0b6ad4e06db0359e09e0a128
Marc Nerenberg <snippii@gmail.com>
2024-03-25 20:00:41 +0100

    Aktualisiere HTML und CSS für Gerätefinder

--- src/js/script.js
+++ src/js/script.js
@@ -2,58 +2,105 @@
 const vehicle = [
 	{
 		name: "Rüstwagen",
-		plate: "",
+		plate: "WL LK 810",
 		equipment: [
-			{ room: "G1", items: ["A", "B2", "C"] },
-			{ room: "G2", items: ["D", "E", "F"] },
+			{
+				room: "G1",
+				image: "src/img/rw_g1.JPEG",
+				items: [
+					"Wathosen",
+					"Motorsäge mit Zubehör",
+					"Rettungssäge",
+					"Doppelkanister",
+					"Motortrennschleifer mit Zubehör",
+					"Trennscheiben",
+					"Schachtabdeckungen",
+					"Dichtungskeile",
+					"Dichtungspfropfen",
+					"Leitungstrommeln",
+					"Kanister 10l",
+					"Ausgußsturzen",
+					"Leitungen mit Stecker und Kupplungen",
+					"Schutzschalter",
+					"Kasten mit Ersatzlampen",
+					"Waldarbeiterhelme",
+					"Beinschutz",
+					"Kupferlitze",
+					"Anschlusszwingen",
+					"Erdungsspieß",
+					"Haftmagnet",
+				],
+			}, // Add a comma here
+			{ room: "G2", image: "src/img/rw_g2.JPEG", items: ["D", "E", "F"] },
 		],
 	},
 	{
 		name: "Tanklöschwagen",
+		plate: "",
+		equipment: [
+			{ room: "G1", image: "", items: ["Z", "Y", "X"] },
+			{ room: "G2", image: "", items: ["V", "W", "L"] },
+		],
 	},
 ];
-// console.log(vehicle);
 
-let html = "";
-let listItemes = "";
-const vehicleItem = vehicle.length;
-const rooms = document.querySelector(".rooms");
-const click = document.querySelector("#add");
+// Funktion zum Erstellen der Tabelle
+function createTable() {
+	let table = `<table class="table table-striped table-bordered">
+	<tbody>`;
 
-const itemsList = (item, index) => {
-	listItemes += `
-    <li class="list-group-item">${item}</li>
-    `;
-	console.log("itemsList: " + listItemes);
-};
+	// Durchlaufen Sie jedes Fahrzeug im Array
+	vehicle.forEach((v) => {
+		table += `<tr class="table-header">
+			<td colspan="2"><strong>${v.name}</strong></td>
+			<td>Kennzeichen: ${v.plate}</td>`;
+		table += `<tr>
+			<th scope="col">Geräteraum</th>
+			<th scope="col">Bild</th>
+			<th scope="col">Liste</th>
+		</tr>`;
+		// Durchlaufen Sie die Ausrüstung jedes Fahrzeugs
+		v.equipment.forEach((e) => {
+			table += `<tr><td>${e.room}</td>`; // Geräteraum
+			table += `<td><img src="${e.image}" class="img-thumbnail rounded-top" alt="" width="200px" onclick="openLightbox('${e.image}')" /></td>`; // Bild
+			table += `<td><ul class="list-group">`; // Liste
+			// Durchlaufen Sie die Artikel in jedem Raum
+			e.items.forEach((i) => {
+				table += `<li class="list-group-item">${i}</li>`; // Artikel
+			});
+			table += "</ul></td></tr>";
+		});
+	});
 
-// for(var i = 0; i < arr.length; i++) {
-//      var n = arr[i];
-//      if (n == 3) {
-//          break;
-//       }
-//       alert(n);
-//    });
+	table += "</tbody></table>";
 
-const itemsG1 = vehicle[0].equipment[0].items.forEach(itemsList);
+	return table;
+}
 
-generateRoomsTemplate = () => {
-	html = `
-        <tr>
-            <th scope="row">${vehicle[0].equipment[0].room}</th>
-            <td>
-                <img src="src/img/rw_g1.JPEG" class="img-thumbnail rounded-top" alt="" width="200px" />
+// Fügen Sie die Tabelle zum HTML-Dokument hinzu
+const table = createTable();
+const tableContainer = document.createElement("div");
+tableContainer.innerHTML = table;
+document.body.appendChild(tableContainer);
 
-            </td>
-            <td>
-            <ul class="list-group">${listItemes}</ul>
-            </td>
-        </tr>
-    `;
+// Funktion zum Öffnen der Lightbox
+function openLightbox(imageSrc) {
+	// Erstellen Sie ein neues div-Element für die Lightbox
+	const lightbox = document.createElement("div");
+	lightbox.id = "lightbox";
 
-	rooms.innerHTML += html;
-};
+	// Erstellen Sie ein neues img-Element für das Bild
+	const img = document.createElement("img");
+	img.src = imageSrc;
 
-click.addEventListener("click", (e) => {
-	generateRoomsTemplate();
-});
+	// Fügen Sie das img-Element zur Lightbox hinzu
+	lightbox.appendChild(img);
+
+	// Fügen Sie die Lightbox zum body-Element hinzu
+	document.body.appendChild(lightbox);
+
+	// Schließen Sie die Lightbox, wenn sie angeklickt wird
+	lightbox.addEventListener("click", () => {
+		document.body.removeChild(lightbox);
+	});
+}
